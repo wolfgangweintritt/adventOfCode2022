7727f796d9c9ee8ab8c79beae3bcf3c680b7db83
Wolfgang Weintritt <w.weintritt@justimmo.at>
2022-12-10 00:28:05 +0100

    Add day 3 part 2

--- day3/solver.py
+++ day3/solver.py
@@ -1,4 +1,4 @@
-def main(filePath: str) -> int:
+def part1(filePath: str) -> int:
     inputFile = open(filePath, 'r')
     lines = inputFile.readlines()
     inputFile.close()
@@ -6,13 +6,28 @@ def main(filePath: str) -> int:
     return sum(commonItems)
 
 
-def splitLine(line: str) -> tuple[str, str]:
+def part2(filePath: str) -> int:
+    inputFile = open(filePath, 'r')
+    lines = inputFile.readlines()
+    inputFile.close()
+    triples = []
+    for c in range(0, len(lines), 3):
+        triples.append(lines[c:c+3])
+    badges = map(lambda triple: itemScore(findCommonItem(triple)), triples)
+    return sum(badges)
+
+
+def splitLine(line: str) -> list[str]:
     splitPoint = int ((len(line.rstrip()))/2)
-    return line[:splitPoint], line[splitPoint:]
+    return [line[:splitPoint], line[splitPoint:]]
 
 
-def findCommonItem(lineSplit: tuple[str, str]) -> str:
-    return next(filter(lambda item: lineSplit[1].find(item) > -1, lineSplit[0]))
+def findCommonItem(itemList: list[str]) -> str:
+    return next(filter(lambda item: findCommonItemList(item, itemList[1:]), itemList[0]))
+
+
+def findCommonItemList(item: str, rucksacks: list[str]) -> bool:
+    return len(list(filter(lambda rucksack: rucksack.find(item) > -1, rucksacks))) == len(rucksacks)
 
 
 def itemScore(item: str) -> int:
@@ -22,4 +37,5 @@ def itemScore(item: str) -> int:
 
 
 if __name__ == '__main__':
-    print(main('input.txt'))
+    print(part1('input.txt'))
+    print(part2('input.txt'))
--- test/day3/solverTest.py
+++ test/day3/solverTest.py
@@ -1,16 +1,20 @@
 import unittest
 
-from day3.solver import main
+from day3.solver import part1, part2
 
 
 class TestDay3(unittest.TestCase):
     def test_small_input_1(self):
-        score = main('Fixtures/small_input_1.txt')
+        score = part1('Fixtures/small_input_1.txt')
         self.assertEqual(157, score)
+        badgesScore = part2('Fixtures/small_input_1.txt')
+        self.assertEqual(70, badgesScore)
 
     def test_small_input_2(self):
-        score = main('Fixtures/small_input_2.txt')
+        score = part1('Fixtures/small_input_2.txt')
         self.assertEqual(104, score)
+        badgesScore = part2('Fixtures/small_input_2.txt')
+        self.assertEqual(51, badgesScore)
 
 
 if __name__ == '__main__':
