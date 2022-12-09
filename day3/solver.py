1711a05dc48d4e7fc8fa8e38d5649870fc634de6
Wolfgang Weintritt <w.weintritt@justimmo.at>
2022-12-09 23:55:55 +0100

    Add day 3 part 1

--- day3/solver.py
+++ day3/solver.py
@@ -1,5 +1,24 @@
 def main(filePath: str) -> int:
-    return 0
+    inputFile = open(filePath, 'r')
+    lines = inputFile.readlines()
+    inputFile.close()
+    commonItems = map(lambda line: itemScore(findCommonItem(splitLine(line))), lines)
+    return sum(commonItems)
+
+
+def splitLine(line: str) -> tuple[str, str]:
+    splitPoint = int ((len(line.rstrip()))/2)
+    return line[:splitPoint], line[splitPoint:]
+
+
+def findCommonItem(lineSplit: tuple[str, str]) -> str:
+    return next(filter(lambda item: lineSplit[1].find(item) > -1, lineSplit[0]))
+
+
+def itemScore(item: str) -> int:
+    intAsciiValue = ord(item[0])
+    # a => 97; A => 65
+    return intAsciiValue - 96 if intAsciiValue >= 97 else intAsciiValue - (64 - 26)
 
 
 if __name__ == '__main__':
