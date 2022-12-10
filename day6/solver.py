099f052831f56c51b83d4cd7885cd8ee1852e1d1
Wolfgang Weintritt <w.weintritt@justimmo.at>
2022-12-10 23:25:35 +0100

    Add day 6 part 1

--- day6/solver.py
+++ day6/solver.py
@@ -1,6 +1,14 @@
 def part1(filePath: str) -> int:
-    pass
+    inputFile = open(filePath, 'r')
+    input = inputFile.read().rstrip()
+    inputFile.close()
 
+    for x in range(0, len(input) - 3):
+        toCheck = input[x:x+4]
+        if len(list(filter(lambda c: toCheck.count(c) > 1, toCheck))) == 0:
+            return x+4
+
+    return -1
 
 if __name__ == '__main__':
     print(part1('input.txt'))
