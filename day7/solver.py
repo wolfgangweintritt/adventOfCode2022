fa86530a0f9cc07d42b75e1c681376172d5f9ceb
Wolfgang Weintritt <w.weintritt@justimmo.at>
2022-12-10 23:53:43 +0100

    Add day 7 tests

---
+++ day7/solver.py
@@ -0,0 +1,6 @@
+def main(filePath: str, maxDirSize: int) -> int:
+    pass
+
+
+if __name__ == '__main__':
+    print(main('input.txt', 10000))
---
+++ test/day7/solverTest.py
@@ -0,0 +1,13 @@
+import unittest
+
+from day7.solver import main
+
+
+class TestDay7(unittest.TestCase):
+    def test_example_input_1(self):
+        dirSizes = main('Fixtures/example_input.txt', 10000)
+        self.assertEqual(95437, dirSizes)
+
+
+if __name__ == '__main__':
+    unittest.main()
