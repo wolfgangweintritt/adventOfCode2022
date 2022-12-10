a517b062a8797a1635210c1d28a0844715a6680a
Wolfgang Weintritt <w.weintritt@justimmo.at>
2022-12-10 23:27:45 +0100

    Add day 6 part 2

--- test/day6/solverTest.py
+++ test/day6/solverTest.py
@@ -1,28 +1,38 @@
 import unittest
 
-from day6.solver import part1
+from day6.solver import main
 
 
 class TestDay6(unittest.TestCase):
     def test_example_input_1(self):
-        markerPosition = part1('Fixtures/example_input_1.txt')
+        markerPosition = main('Fixtures/example_input_1.txt', 4)
         self.assertEqual(7, markerPosition)
+        markerPosition = main('Fixtures/example_input_1.txt', 14)
+        self.assertEqual(19, markerPosition)
 
     def test_example_input_2(self):
-        markerPosition = part1('Fixtures/example_input_2.txt')
+        markerPosition = main('Fixtures/example_input_2.txt', 4)
         self.assertEqual(5, markerPosition)
+        markerPosition = main('Fixtures/example_input_2.txt', 14)
+        self.assertEqual(23, markerPosition)
 
     def test_example_input_3(self):
-        markerPosition = part1('Fixtures/example_input_3.txt')
+        markerPosition = main('Fixtures/example_input_3.txt', 4)
         self.assertEqual(6, markerPosition)
+        markerPosition = main('Fixtures/example_input_3.txt', 14)
+        self.assertEqual(23, markerPosition)
 
     def test_example_input_4(self):
-        markerPosition = part1('Fixtures/example_input_4.txt')
+        markerPosition = main('Fixtures/example_input_4.txt', 4)
         self.assertEqual(10, markerPosition)
+        markerPosition = main('Fixtures/example_input_4.txt', 14)
+        self.assertEqual(29, markerPosition)
 
     def test_example_input_5(self):
-        markerPosition = part1('Fixtures/example_input_5.txt')
+        markerPosition = main('Fixtures/example_input_5.txt', 4)
         self.assertEqual(11, markerPosition)
+        markerPosition = main('Fixtures/example_input_5.txt', 14)
+        self.assertEqual(26, markerPosition)
 
 
 if __name__ == '__main__':
