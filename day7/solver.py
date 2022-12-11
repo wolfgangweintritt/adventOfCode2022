023b871948aed454a2b5f7fb05ead947d1553279
Wolfgang Weintritt <w.weintritt@justimmo.at>
2022-12-11 23:16:30 +0100

    Add day 7 part 2

--- day7/solver.py
+++ day7/solver.py
@@ -1,9 +1,24 @@
 import re
+from typing import Optional
 
 from day7.value.file import Dir, File
 
 
-def main(filePath: str, maxDirSize: int) -> int:
+def part1(filePath: str, maxDirSize: int) -> int:
+    rootDir = createDirStructure(filePath)
+
+    return sumDirsWithSizeAtMost(rootDir, maxDirSize)
+
+
+def part2(filePath: str, diskSpace: int, unusedSpaceNeeded: int) -> int:
+    rootDir = createDirStructure(filePath)
+    currentlyUnusedSpace = diskSpace - rootDir.getSize()
+    unusedSpaceDiff = unusedSpaceNeeded - currentlyUnusedSpace
+
+    return findSmallestDirGteMinSize(rootDir, unusedSpaceDiff).getSize()
+
+
+def createDirStructure(filePath: str) -> Dir:
     with open(filePath) as f:
         lines = f.read().splitlines()
 
@@ -24,18 +39,29 @@ def main(filePath: str, maxDirSize: int) -> int:
         elif line.startswith('dir '): # subdir
             currentDir.addChild(Dir(line[4:], currentDir))
 
-    return sumDirsWithSizeAtMost(rootDir, maxDirSize)
+    return rootDir
 
-def sumDirsWithSizeAtMost(d: Dir, maxSize: int):
+
+def sumDirsWithSizeAtMost(d: Dir, maxSize: int) -> int:
     sizeSum = 0
     if d.getSize() <= maxSize:
         sizeSum += d.getSize()
-    for s in d.getSubDirs():
-        sizeSum += sumDirsWithSizeAtMost(s, maxSize)
+    for subdir in d.getSubDirs():
+        sizeSum += sumDirsWithSizeAtMost(subdir, maxSize)
 
     return sizeSum
 
 
+def findSmallestDirGteMinSize(d: Dir, minDirSize: int) -> Optional['Dir']:
+    minDir = d
+    for subdir in d.getSubDirs():
+        smallestSubdirGteMinSize = findSmallestDirGteMinSize(subdir, minDirSize)
+        if (smallestSubdirGteMinSize != None and smallestSubdirGteMinSize.getSize() < minDir.getSize() and smallestSubdirGteMinSize.getSize() >= minDirSize):
+            minDir = smallestSubdirGteMinSize
+
+    return minDir if minDir.getSize() >= minDirSize else None
+
 
 if __name__ == '__main__':
-    print(main('input.txt', 100000))
+    print(part1('input.txt', 100000))
+    print(part2('input.txt', 70000000, 30000000))
--- test/day7/solverTest.py
+++ test/day7/solverTest.py
@@ -1,13 +1,28 @@
 import unittest
 
-from day7.solver import main
+from day7.solver import part1, part2
 
 
 class TestDay7(unittest.TestCase):
     def test_example_input_1(self):
-        dirSizes = main('Fixtures/example_input.txt', 100000)
+        dirSizes = part1('Fixtures/example_input.txt', 100000)
         self.assertEqual(95437, dirSizes)
+        smallestDirGteMinSize = part2('Fixtures/example_input.txt', 70000000, 30000000)
+        self.assertEqual(24933642, smallestDirGteMinSize)
 
+    def test_simple_input_1(self):
+        smallestDirGteMinSize = part2('Fixtures/simple_input_1.txt', 30, 4)
+        self.assertEqual(4, smallestDirGteMinSize)
+        smallestDirGteMinSize = part2('Fixtures/simple_input_1.txt', 41, 20)
+        self.assertEqual(10, smallestDirGteMinSize)
+        smallestDirGteMinSize = part2('Fixtures/simple_input_1.txt', 40, 20)
+        self.assertEqual(10, smallestDirGteMinSize)
+        smallestDirGteMinSize = part2('Fixtures/simple_input_1.txt', 39, 20)
+        self.assertEqual(11, smallestDirGteMinSize)
+        smallestDirGteMinSize = part2('Fixtures/simple_input_1.txt', 38, 20)
+        self.assertEqual(13, smallestDirGteMinSize)
+        smallestDirGteMinSize = part2('Fixtures/simple_input_1.txt', 37, 20)
+        self.assertEqual(13, smallestDirGteMinSize)
 
 if __name__ == '__main__':
     unittest.main()
