6aa727186767d7367cfb31f483c4a095296159b6
Wolfgang Weintritt <w.weintritt@justimmo.at>
2022-12-15 00:48:51 +0100

    Add day 9 part 2

--- day9/solver.py
+++ day9/solver.py
@@ -21,5 +21,28 @@ def part1(filePath: str) -> int:
     return len(visitedTailPositions)
 
 
+def part2(filePath: str) -> int:
+    with open(filePath) as f:
+        lines = f.read().splitlines()
+
+    moves = map(lambda line: Move.fromLine(line), lines)
+
+    ropePositions = [Position(0, 0) for i in range(10)]
+    visitedTailPositions = {ropePositions[-1].toString()}
+
+    for move in moves:
+        for i in range(move.steps):
+            for knotIdx in range(len(ropePositions)):
+                if (knotIdx == 0):
+                    ropePositions[knotIdx].moveToDirection(move.direction)
+                else:
+                    ropePositions[knotIdx].moveAccordingToHeadPosition(ropePositions[knotIdx - 1])
+
+            visitedTailPositions.add(ropePositions[-1].toString())
+
+    return len(visitedTailPositions)
+
+
 if __name__ == '__main__':
     print(part1('input.txt'))
+    print(part2('input.txt'))
--- test/day9/solverTest.py
+++ test/day9/solverTest.py
@@ -1,12 +1,16 @@
 import unittest
 
-from day9.solver import part1
+from day9.solver import part1, part2
 
 
 class TestDay9(unittest.TestCase):
     def test_example_input_1(self):
         tailVisited = part1('Fixtures/example_input.txt')
         self.assertEqual(13, tailVisited)
+
+    def test_example_input_part2(self):
+        tailVisited = part2('Fixtures/example_input_part2.txt')
+        self.assertEqual(36, tailVisited)
 
 
 if __name__ == '__main__':
