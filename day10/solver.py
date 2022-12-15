c5e0ba342dd4674c4892a4802c3488c56abab41a
Wolfgang Weintritt <w.weintritt@justimmo.at>
2022-12-16 00:07:51 +0100

    Add day 10 part 2

--- day10/solver.py
+++ day10/solver.py
@@ -17,9 +17,52 @@ def part1(filePath: str) -> int:
 
     return reduce(
         lambda carry, value: carry + (value[1] * value[0]) if value[0] == 20 or (value[0] - 20) % 40 == 0 else carry,
-        zip(list(range(1, len(registerHistory) + 1)), registerHistory), # tuple (cycle (starting with 1), registerValue)
+        zip(list(range(1, len(registerHistory) + 1)), registerHistory),  # tuple (cycle (starting with 1), registerValue)
         0
     )
+
+
+def part2(filePath: str) -> str:
+    with open(filePath) as f:
+        lines = f.read().splitlines()
+
+    moves = map(lambda line: Instruction.fromLine(line), lines)
+
+    crt = CRT(40, 1)
+    for move in moves:
+        for c in range(move.cycles):
+            crt.drawPixel()
+            if c == move.cycles - 1:
+                crt.moveSprite(move.addToRegister)
+
+    return crt.drawMonitor()
+
+
+class CRT:
+    pixels = []
+
+    def __init__(self, screenSize: int, spritePosition: int) -> None:
+        self.screenSize = screenSize
+        self.spritePosition = spritePosition
+
+    def drawPixel(self) -> None:
+        currentLinePixelIdx = len(self.pixels) % self.screenSize
+        if abs(currentLinePixelIdx - self.spritePosition) <= 1:
+            self.pixels.append('#')
+        else:
+            self.pixels.append('.')
+
+    def moveSprite(self, addToSpritePosition):
+        self.spritePosition += addToSpritePosition
+
+    def drawMonitor(self) -> str:
+        monitor = ''
+        for idx, pixel in enumerate(self.pixels):
+            monitor += pixel
+            if (idx + 1) % self.screenSize == 0:
+                monitor += '\n'
+
+        return monitor
 
 
 class Instruction:
@@ -37,3 +80,4 @@ class Instruction:
 
 if __name__ == '__main__':
     print(part1('input.txt'))
+    print(part2('input.txt'))
--- test/day10/solverTest.py
+++ test/day10/solverTest.py
@@ -1,13 +1,20 @@
 import unittest
 
-from day10.solver import part1
+from day10.solver import part1, part2
 
 
 class TestDay10(unittest.TestCase):
     def test_example_input_1(self):
         signalStrengthSum = part1('Fixtures/example_input.txt')
         self.assertEqual(13140, signalStrengthSum)
-
+        monitorOutput = part2('Fixtures/example_input.txt')
+        self.assertEqual("""##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+""", monitorOutput)
 
 if __name__ == '__main__':
     unittest.main()
