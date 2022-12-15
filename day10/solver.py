7cd7bd8f36413fbe3b7526d27365ab40e1a61fb3
Wolfgang Weintritt <w.weintritt@justimmo.at>
2022-12-15 23:34:34 +0100

    Add day 10 part 1

--- day10/solver.py
+++ day10/solver.py
@@ -1,7 +1,38 @@
+from functools import reduce
+
+
 def part1(filePath: str) -> int:
-    # consider signal strength (the cycle number multiplied by the value of the X register) during the
-    # 20th cycle and every 40 cycles after that (that is, during the 20th, 60th, 100th, 140th, 180th, and 220th cycles).
-    pass
+    with open(filePath) as f:
+        lines = f.read().splitlines()
+
+    moves = map(lambda line: Instruction.fromLine(line), lines)
+
+    registerHistory = []
+    registerValue = 1
+    for move in moves:
+        for c in range(move.cycles):
+            registerHistory.append(registerValue)
+            if c == move.cycles - 1:
+                registerValue += move.addToRegister
+
+    return reduce(
+        lambda carry, value: carry + (value[1] * value[0]) if value[0] == 20 or (value[0] - 20) % 40 == 0 else carry,
+        zip(list(range(1, len(registerHistory) + 1)), registerHistory), # tuple (cycle (starting with 1), registerValue)
+        0
+    )
+
+
+class Instruction:
+    def __init__(self, cycles: int, addToRegister: int) -> None:
+        self.cycles = cycles
+        self.addToRegister = addToRegister
+
+    @classmethod
+    def fromLine(cls, line: str) -> 'Instruction':
+        lineSplit = line.split(' ')
+        if len(lineSplit) == 1:
+            return Instruction(1, 0)
+        return Instruction(2, int(lineSplit[1]))
 
 
 if __name__ == '__main__':
